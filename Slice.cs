9fd541d8488351ab077357c147ad9cd26c4f5b8b
James <jameshaller27@gmail.com>
2023-04-07 23:06:00 -0700

    Use TemplateMethod pattern for Slice state

--- Slice.cs
+++ Slice.cs
@@ -1,6 +1,5 @@
 using Godot;
 
-
 namespace SimonSays;
 
 public partial class Slice : Node2D
@@ -8,14 +7,59 @@ public partial class Slice : Node2D
 	[Export] private Color ActiveColor { get; set; }
 	[Export] private Color InactiveColor { get; set; }
 
+	private State CurrentState { get; set; }
+
 	public override void _Ready()
 	{
 		base._Ready();
-		this.Modulate = this.InactiveColor;
+		this.SetState(new InactiveState(this));
 	}
 
 	public void SetActive(bool isActive)
 	{
-		this.Modulate = isActive ? ActiveColor : InactiveColor;
+		this.SetState(isActive ? new ActiveState(this) : new InactiveState(this));
+	}
+
+	private void SetState(State state)
+	{
+		this.CurrentState = state;
+		this.CurrentState.Activate();
+	}
+
+	// States
+
+	private abstract class State
+	{
+		protected Slice Slice { get; }
+
+		public State(Slice slice)
+		{
+			this.Slice = slice;
+		}
+
+		public void Activate()
+		{
+			this.Slice.Modulate = this.Color;
+		}
+
+		protected abstract Color Color { get; }
+	}
+
+	private class ActiveState : State
+	{
+		public ActiveState(Slice slice) : base(slice)
+		{
+		}
+
+		protected override Color Color => this.Slice.ActiveColor;
+	}
+
+	private class InactiveState : State
+	{
+		public InactiveState(Slice slice) : base(slice)
+		{
+		}
+
+		protected override Color Color => this.Slice.InactiveColor;
 	}
 }
